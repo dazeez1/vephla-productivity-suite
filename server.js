7e737a7556748480f87e410bf7214582295830a1
dazeez1 <d.azeez@alustudent.com>
2026-01-05 01:18:35 +0200

    chore: scaffold backend folder structure

---
+++ server.js
@@ -0,0 +1,5 @@
+// Entry point for the Express server
+// TODO: Initialize Express app, connect to MongoDB, set up routes, start server
+
+console.log('Server file created - ready for implementation');
+
